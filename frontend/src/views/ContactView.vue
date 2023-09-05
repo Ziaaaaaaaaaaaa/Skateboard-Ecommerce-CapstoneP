cd1045a695501c09bb99d3e74d766a116f1bd016
Ziaaaaaaaaaaaa <ziakhan29042003@gmail.com>
2023-09-05 11:17:02 +0200

    commit

--- frontend/src/views/ContactView.vue
+++ frontend/src/views/ContactView.vue
@@ -1,6 +1,43 @@
 <template>
     <div>
+        <div class="heading">
+            <h1 class="heading-text text-white">Contact Page</h1>
+          </div>       
+           <div class="container">
+        <div class="heading-1">
 
+                <h1 class=" text text-white">Contact us</h1>
+            </div>
+            <form>
+                <div class="row">
+                    <div class="col-6">
+                        <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label text-white">Full Name</label>
+                      <input type="email" class="form-control bg-dark" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleInputEmail1" class="form-label text-white">Email address</label>
+                      <input type="email" class="form-control bg-dark" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+                    </div>
+                    <div class="mb-3">
+                      <label for="exampleInputPassword1" class="form-label text-white">Message</label>
+                      <textarea name="" id="texta" cols="90" rows="5" class="bg-dark w-100" required></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-outline-light">Submit</button>
+                    </div>
+                    <div class="col-6">
+                      <h3>
+                          Contact
+                      </h3>
+                      <p>Zia@gmail.com</p>
+                      <h3>
+                          Location
+                      </h3>
+                      <p>Landsdown, Life Choices Academy</p>
+                    </div>
+                </div>
+              </form>
+        </div>
     </div>
 </template>
 
@@ -11,5 +48,68 @@
 </script>
 
 <style scoped>
+
+.container{
+    background-color: rgb(52, 52, 52);
+    width: 50%;
+    position: relative;
+    bottom: 20rem;
+    border-radius: 5%;
+    padding-bottom: 4rem;
+}
+
+.side-text {
+    font-size: 7rem;
+    color: white;
+    transform: rotate(-90deg);
+    position: fixed;
+    right: 73rem;
+    bottom: 15rem;
+    font-weight: 200;
+    z-index: 2;
+  }
+
+.heading-1{
+ padding-block: 3rem;
+}
+
+.text{
+    font-size: 4.5rem;
+}
+
+h3{
+    color: white;
+}
+
+p{
+    color: white;
+}
+
+#exampleInputEmail1 {
+    color: rgb(255, 255, 255);
+}
+
+#texta {
+    color: blue;
+}
+
+.heading{
+    border-radius: 20px;
+    background-color: rgba(24, 24, 24, 0.58);
+    position: relative;
+    z-index: 1;
+    bottom: 32rem;
+    left: 27rem;
+    padding-inline: 2rem;
+    padding-block: 2rem;
+    margin-right: 51.5rem;
+  
+  }
+  
+  .heading-text{
+    font-size: 7rem;
+  }
+
+
 
 </style>
